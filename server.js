c797e16359e5d1e4455ae31e0b733574f429a30f
shruti-4 <shruti.arora1307@gmail.com>
2018-07-23 16:53:03 +0530

    hello

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine','hbs' );
@@ -53,4 +54,6 @@ app.get('/bad', (req,res) => {
         errorMessage: 'unable to ahndele'
     });
 });
-app.listen(3000);
+app.listen(port,() =>{
+    console.log(`server is at ${port}`);
+});
